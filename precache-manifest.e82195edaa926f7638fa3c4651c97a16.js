25e280e899b385a51d74756c924fb2bd876f1f0d
ColdFennec <xColdLight@Outlook.com>
2021-02-04 13:39:39 +0100

    Add first version of Fetch That Joke from dist project

---
+++ precache-manifest.e82195edaa926f7638fa3c4651c97a16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e40efb6ca3db9af7643",
+    "url": "/css/about.ef9c5391.css"
+  },
+  {
+    "revision": "90c6f863fd5449068d33",
+    "url": "/css/app.c88169f1.css"
+  },
+  {
+    "revision": "75a020481b013e0c6cfe",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "c495d6df2119ac0ab5666c7845b5315c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e40efb6ca3db9af7643",
+    "url": "/js/about.b164499e.js"
+  },
+  {
+    "revision": "90c6f863fd5449068d33",
+    "url": "/js/app.4f7183ce.js"
+  },
+  {
+    "revision": "75a020481b013e0c6cfe",
+    "url": "/js/chunk-vendors.57cafea2.js"
+  },
+  {
+    "revision": "2c8c301462329560b38395c4c5cb23c6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
